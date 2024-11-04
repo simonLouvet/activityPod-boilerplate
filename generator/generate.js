19400a47bf09b1704c75710c33aa87aa090518b5
Alice Poggioli <alice.poggioli@assemblee-virtuelle.org>
2024-11-04 15:01:16 +0100

    create relationship and WIP:clean data before running the script

--- generator/generate.js
+++ generator/generate.js
@@ -1,5 +1,6 @@
 const { ACTIVITY_TYPES } = require('@semapps/activitypub');
 const { ServiceBroker } = require('moleculer');
+const Redis = require('ioredis');
 const CONFIG = require('./config');
 const RdfJSONSerializer = require('./RdfJSONSerializer.js');
 
@@ -28,9 +29,74 @@ const connectPodProvider = async () => {
   return broker;
 };
 
+const listDatasets = async () => {
+  const response = await fetch(`${CONFIG.SPARQL_ENDPOINT}$/datasets`, {
+    headers: {
+      Authorization: `Basic ${Buffer.from(`${CONFIG.JENA_USER}:${CONFIG.JENA_PASSWORD}`).toString('base64')}`
+    }
+  });
+
+  if (response.ok) {
+    const json = await response.json();
+    return json.datasets.map(dataset => dataset['ds.name'].substring(1));
+  }
+  return [];
+};
+
+const clearMails = async () => {
+  await fetch(CONFIG.MAILCATCHER_API_URL, {
+    method: 'DELETE'
+  });
+};
+
+const clearDataset = async dataset => {
+  const response = await fetch(`${CONFIG.SPARQL_ENDPOINT + dataset}/update`, {
+    method: 'POST',
+    body: 'update=CLEAR+ALL', // DROP+ALL is not working with WebACL datasets !
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${Buffer.from(`${CONFIG.JENA_USER}:${CONFIG.JENA_PASSWORD}`).toString('base64')}`
+    }
+  });
+  return response;
+};
+
+const clearRedisDb = async redisUrl => {
+  const redisClient = new Redis(redisUrl);
+  await redisClient.flushdb();
+  redisClient.disconnect();
+};
+
+const clearAllData = async () => {
+  try {
+    const datasets = await listDatasets();
+    console.log('Datasets trouvés:', datasets);
+
+    for (let dataset of datasets) {
+      const response = await clearDataset(dataset);
+      if (!response.ok) {
+        const text = await response.text();
+        console.error(`Erreur lors du nettoyage du dataset ${dataset}:`, text);
+      } else {
+        console.log("Ce jeu a été supprimé : ", dataset)
+        console.log("La réponse était : ", response)
+      }
+    }
+
+    await clearRedisDb(CONFIG.QUEUE_SERVICE_URL);
+    await clearRedisDb(CONFIG.REDIS_OIDC_PROVIDER_URL);
+
+    await clearMails();
+    console.log('Nettoyage terminé');
+  } catch (error) {
+    console.error('Erreur lors du nettoyage:', error);
+  }
+};
+
 // find in tests/pods-creation.test.js
 //https://developer.mozilla.org/en-US/docs/Glossary/IIFE
 (async () => {
+  await clearAllData();
   const NUM_PODS = 2;
   const podProvider = await connectPodProvider();
   const actors = [];
@@ -53,8 +119,7 @@ const connectPodProvider = async () => {
         ...options,
         meta: { ...options.meta, webId, dataset: actors[i].preferredUsername }
       });
-    console.log(actors[i]);
-
+    // console.log(actors[i]);
   }
 
   const nick = actors[1];
@@ -73,7 +138,7 @@ const connectPodProvider = async () => {
     to: anastasia.id
   });
 
-  await anastasia.call('activitypub.outbox.post', {
+  const contactRequestAcceptedByAnastasia = await anastasia.call('activitypub.outbox.post', {
     collectionUri: anastasia.outbox,
     type: ACTIVITY_TYPES.ACCEPT,
     actor: anastasia.id,
@@ -81,5 +146,7 @@ const connectPodProvider = async () => {
     to: nick.id
   });
 
+  console.log('contactRequestToAnastasia', contactRequestToAnastasia);
+  console.log('contactRequestAcceptedByAnastasia', contactRequestAcceptedByAnastasia);
   return actors;
 })();
